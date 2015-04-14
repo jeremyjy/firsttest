9cafafb145955e96af34cae6a9fc5462df5cbe64
JeremyNg <jeremyjy@163.com>
2015-04-14 12:45:32 +0800

    xxx

--- cloud/app.js
+++ cloud/app.js
@@ -10,10 +10,10 @@ var Comments = require('cloud/mComment.js')
 app.set('views', 'cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
-app.use(express.cookieSession());
 
 // 启用 cookieParser
 app.use(express.cookieParser('Your Cookie Secure'));
+app.use(express.cookieSession())
 // 使用 avos-express-cookie-session 记录登录信息到 cookie
 app.use(avosExpressCookieSession({cookie: {maxAge: 3600000}, fetchUser: true}));
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
